6fbd6df295301003f85d0f877fa1771a03450e35
hyerim0518 <hh654@cornell.edu>
2023-01-18 12:09:00 -0500

    Learned how to join

--- code/un-dataanalysis.R
+++ code/un-dataanalysis.R
@@ -23,4 +23,51 @@ gapminder_data %>%
 gapminder_data %>%
   filter(year == 1952) %>%
   summarize(average_gdp=mean(gdpPercap))
-  
+
+#group by
+gapminder_data %>%
+  group_by(year, continent) %>%
+  summarize(average=mean(lifeExp),
+            error = sd(lifeExp))
+#mutate
+
+gapminder_data %>%
+  mutate(gdp = pop * gdpPercap)
+
+gapminder_data %>%
+  mutate(popInMillions = pop/1000000)
+
+#select
+gapminder_data %>%
+  select(pop, year)
+
+gapminder_data %>%
+  select(-continent, pop)
+
+gapminder_data %>%
+  select(country, continent, year, lifeExp) %>%
+  pivot_wider(names_from = year, values_from = lifeExp )%>%
+  view()
+
+# 
+co2_emissions_dirty <- read_csv("co2-un-data.csv", skip=2,
+                                col_names=c("region", "country", "year", "series", "value", "footnotes", "source"))
+
+co2_emissions<-co2_emissions_dirty %>% 
+  select(country, year, series, value) %>%
+  mutate(series = recode(series, "Emissions (thousand metric tons of carbon dioxide)" = "total_emissions",
+                         "Emissions per capita (metric tons of carbon dioxide)" = "per_capita_emissions"))%>%
+  pivot_wider(names_from=series, values_from=value)%>%
+  filter(year == 2005) %>%
+  select(-year)
+
+# bring in 2007 population data
+gapminder_data_2007 <- read_csv("data/gapminder_data.csv")%>%
+filter(year == 2007)%>%
+select(country, pop, lifeExp, gdpPercap)
+
+inner_join(co2_emissions, gapminder_data_2007, by = "country")
+
+anti_join(gapminder_data_2007, co2_emissions, by="country")
+
+full_join(co2_emissions, gapminder_data_2007)
